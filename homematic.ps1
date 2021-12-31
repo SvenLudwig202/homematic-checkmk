7c3bc1a228fae5e6e372a9c7d6815642084c85c3
SvenLudwig202 <34524443+SvenLudwig202@users.noreply.github.com>
2021-12-31 12:39:47 +0100

    added OPERATING_VOLTAGE as metric and LOW_BAT for alerting.

--- homematic.ps1
+++ homematic.ps1
@@ -50,9 +50,46 @@ $doc.stateList.device | Select-Xml -XPath "channel/datapoint" | Where-Object { $
   (Get-DeviceItem($device.name)).data += @{"HUMIDITY" = $channel1.value}
 }
 
+#Parse for OPERATING_VOLTAGE
+$doc.stateList.device | Select-Xml -XPath "channel/datapoint" | Where-Object { $_.Node.type -eq 'OPERATING_VOLTAGE' } | Foreach-Object {
+  $channel1 = $_.Node
+  $device = $channel1.ParentNode.ParentNode
+
+  (Get-DeviceItem($device.name)).data += @{"OPERATING_VOLTAGE" = $channel1.value}
+}
+
+#Parse for LOW_BAT
+$doc.stateList.device | Select-Xml -XPath "channel/datapoint" | Where-Object { $_.Node.type -eq 'LOW_BAT' } | Foreach-Object {
+  $channel1 = $_.Node
+  $device = $channel1.ParentNode.ParentNode
+
+  (Get-DeviceItem($device.name)).data += @{"LOW_BAT" = $channel1.value}
+}
+
 $date = Get-Date
 
 $Output | ForEach-Object {
-  $values = ($_.data.GetEnumerator() | Sort-Object -Property Name | ForEach-Object { ("{0}={1:n1}" -f $_.Name, [convert]::ToDouble($_.Value)) })
-  Write-Output ('0 "{0}" {1} {2} - Data updated at: {3}' -f $_.name, ($values -join "|"), ($values -join " "), $date)
+  $retval = 0
+  $metrics = @()
+  $values = @()
+  $_.data.GetEnumerator() | Sort-Object -Property Name | ForEach-Object {
+    $Property = $_
+    switch -regex ($Property.Name) {
+      "^(ACTUAL_TEMPERATURE|SET_POINT_TEMPERATURE|HUMIDITY|OPERATING_VOLTAGE)$" {
+        $metrics += ("{0}={1:n1}" -f $Property.Name, [convert]::ToDouble($Property.Value))
+        $values += ("{0}={1:n1}" -f $Property.Name, [convert]::ToDouble($Property.Value))
+      }
+      "^(LOW_BAT)$" {
+        $values += ("{0}={1:s}" -f $Property.Name, $Property.Value)
+
+        if ($Property.Name -eq "LOW_BAT" -And $Property.Value -eq "true") {
+          $retval = 2
+        }
+      }
+    }
+  }
+  # $metrics
+  # $values
+  # $values = ($_.data.GetEnumerator() | Sort-Object -Property Name | ForEach-Object { ("{0}={1:n1}" -f $_.Name, [convert]::ToDouble($_.Value)) })
+  Write-Output ('{0} "{1}" {2} {3} - Data updated at: {4}' -f $retval, $_.name, ($metrics -join "|"), ($values -join " "), $date)
 }
